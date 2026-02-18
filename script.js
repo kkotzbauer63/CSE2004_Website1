dbb1f137feac6a6b98337a3c847628e3c5dee263
Kevin Kotzbauer <111398309+kkotzbauer63@users.noreply.github.com>
2026-02-17 23:26:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 
 const toggleButton = document.querySelector('#toggleTableBtn');
 const eqTable = document.querySelector('#eqTable');
+const preamp = document.querySelector('#preamp');
 
 toggleButton.addEventListener('click', (event) => {
     console.log('Button clicked:', event);
 
-    //hide or display table
+    //hide or display table and preamp
     eqTable.classList.toggle('hidden');
+    preamp.classList.toggle('hidden');
 
-    //swap text based on waht is displayed
+    //swap text based on what is displayed
     if (eqTable.classList.contains('hidden')) {
         toggleButton.textContent = 'Show EQ Table';
     } else {
